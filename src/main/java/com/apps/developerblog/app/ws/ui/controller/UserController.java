93d34d5ea60fb4bb4259c0785dc10126e6ef54c2
gggray000 <ggg_108@hotmail.com>
2025-02-18 15:01:35 +0800

    Until Video No.25

--- src/main/java/com/apps/developerblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/apps/developerblog/app/ws/ui/controller/UserController.java
@@ -1,19 +1,46 @@
 package com.apps.developerblog.app.ws.ui.controller;
 
+import com.apps.developerblog.app.ws.ui.model.request.UserDetailRequestModel;
+import com.apps.developerblog.app.ws.ui.model.response.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
-    @GetMapping
-    public String getUsers(){
-        return "get user was called";
+    @GetMapping(path="/{userId}", produces = {MediaType.APPLICATION_XML_VALUE,
+                                                MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId){
+        UserRest returnedUser = new UserRest("John", "Doe", "test@test.com");
+        return new ResponseEntity<UserRest>(returnedUser, HttpStatus.OK);
     }
 
-    @PostMapping
-    public String createUser(){
-        return "create user was called";
+    @GetMapping
+    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
+                           @RequestParam(value="limit", defaultValue = "20") int limit,
+                           @RequestParam(value = "sort", required = false) String sort){
+        return "get users was called with page = " + page + ", limit = " + limit + ", sort = " + sort;
+    }
+
+    @PostMapping(consumes = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    },
+            produces = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public UserRest createUser(@RequestBody UserDetailRequestModel userDetails){
+
+        return new UserRest(userDetails.getFirstName(),
+                            userDetails.getLastName(),
+                            userDetails.getEmail()
+        );
     }
 
     @PutMapping
