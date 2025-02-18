b743191f65962836e99c0a2d2d789a2a37da0585
gggray000 <ggg_108@hotmail.com>
2025-02-18 22:04:07 +0800

    Until Video No.30

--- src/main/java/com/apps/developerblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/apps/developerblog/app/ws/ui/controller/UserController.java
@@ -1,29 +1,41 @@
 package com.apps.developerblog.app.ws.ui.controller;
 
+import com.apps.developerblog.app.ws.ui.model.request.UpdateUserDetailsModel;
 import com.apps.developerblog.app.ws.ui.model.request.UserDetailRequestModel;
 import com.apps.developerblog.app.ws.ui.model.response.UserRest;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
-    @GetMapping(path="/{userId}", produces = {MediaType.APPLICATION_XML_VALUE,
-                                                MediaType.APPLICATION_JSON_VALUE})
+    Map<String, UserRest> users;
+
+    @GetMapping(path="/{userId}",
+                produces = {MediaType.APPLICATION_XML_VALUE,
+                            MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<UserRest> getUser(@PathVariable String userId){
-        UserRest returnedUser = new UserRest("John", "Doe", "test@test.com");
-        return new ResponseEntity<UserRest>(returnedUser, HttpStatus.OK);
+        if(users.containsKey(userId)){
+            return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 
     @GetMapping
     public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
                            @RequestParam(value="limit", defaultValue = "20") int limit,
                            @RequestParam(value = "sort", required = false) String sort){
+        String name = null;
+        int length = name.length();
         return "get users was called with page = " + page + ", limit = " + limit + ", sort = " + sort;
     }
 
@@ -35,21 +47,42 @@ public class UserController {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE
     })
-    public UserRest createUser(@RequestBody UserDetailRequestModel userDetails){
-
-        return new UserRest(userDetails.getFirstName(),
-                            userDetails.getLastName(),
-                            userDetails.getEmail()
-        );
+    public UserRest createUser(
+            @Valid
+            @RequestBody UserDetailRequestModel userDetails){
+        UserRest returnedUser = new UserRest(userDetails.getFirstName(),
+                userDetails.getLastName(),
+                userDetails.getEmail());
+        String userId = UUID.randomUUID().toString();
+        returnedUser.setUserId(userId);
+        if(users == null) users = new HashMap<>();
+        users.put(userId, returnedUser);
+        return returnedUser;
     }
 
-    @PutMapping
-    public String updateUser(){
-        return "update user was called";
+    @PutMapping(path="/{userId}",
+                consumes = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+    },
+                produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public UserRest updateUser(@PathVariable String userId,
+                             @Valid
+                             @RequestBody UpdateUserDetailsModel userDetails){
+        UserRest storedUserDetails = users.get(userId);
+        storedUserDetails.setFirstName(userDetails.getFirstName());
+        storedUserDetails.setLastName(userDetails.getLastName());
+        users.put(userId, storedUserDetails);
+        return storedUserDetails;
     }
 
-    @DeleteMapping
-    public String deleteUser(){
-        return "delete user was called";
+    @DeleteMapping(path="/{userId}")
+    public ResponseEntity deleteUser(@PathVariable String userId){
+        users.remove(userId);
+        return ResponseEntity.noContent().build();
+
     }
 }
--- src/main/java/com/apps/developerblog/app/ws/ui/model/request/UpdateUserDetailsModel.java
+++ src/main/java/com/apps/developerblog/app/ws/ui/model/request/UpdateUserDetailsModel.java
@@ -1,22 +1,15 @@
 package com.apps.developerblog.app.ws.ui.model.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class UserDetailRequestModel {
+public class UpdateUserDetailsModel {
     @NotNull(message="First name cannot be null")
     @Size(min=2, message="First name must not be less than 2 chars.")
     private String firstName;
     @NotNull(message="Last name cannot be null")
     @Size(min=2, message="Last name must not be less than 2 chars.")
     private String lastName;
-    @NotNull(message="Email name cannot be null")
-    @Email
-    private String email;
-    @NotNull(message="Password cannot be null")
-    @Size(min=8, max=16, message="Password must be equal or greater than 9 chars and less than 16 chars. ")
-    private String password;
 
     public String getFirstName() {
         return firstName;
@@ -32,21 +25,5 @@ public class UserDetailRequestModel {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
