e6d2b222f9c138b4b845815374578b309f2a5c88
gggray000 <ggg_108@hotmail.com>
2025-02-20 00:23:37 +0800

    Initial Commit of Photo App

---
+++ PhotoAppApiAccountManagement/src/test/java/com/appsdeveloperbolg/photoapp/api/account/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperbolg.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
