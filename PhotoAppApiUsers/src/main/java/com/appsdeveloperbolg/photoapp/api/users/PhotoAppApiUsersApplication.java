bf1bd4dc7bf0055bdf6bdf124022341c76380dba
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-03-06 14:51:40 +0800

    Until video No.267

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperbolg/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperbolg/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,6 +2,8 @@ package com.appsdeveloperbolg.photoapp.api.users;
 
 import com.appsdeveloperbolg.photoapp.api.users.shared.FeignErrorDecoder;
 import feign.Logger;
+import jakarta.persistence.Access;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
@@ -11,6 +13,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,6 +26,9 @@ public class PhotoAppApiUsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
 	}
+
+	@Autowired
+	Environment environment;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -40,8 +47,36 @@ public class PhotoAppApiUsersApplication {
 	}
 
 	@Bean
+	@Profile("production")
 	Logger.Level feignLoggerProducer(){
+		return Logger.Level.NONE;
+	}
+
+	@Bean
+	@Profile("!production")
+	Logger.Level feignDefaultLoggerProducer(){
 		return Logger.Level.FULL;
+	}
+
+	@Bean
+	@Profile("production")
+	public String createProductionBean(){
+		System.out.println("Production bean created. Environment: " + environment.getProperty("myapplication.environment"));
+		return "Production bean";
+	}
+
+	@Bean
+	@Profile("default")
+	public String createDevelopmentBean(){
+		System.out.println("Development bean created. Environment: " + environment.getProperty("myapplication.environment"));
+		return "Development bean";
+	}
+
+	@Bean
+	@Profile("!production")
+	public String createNotProductionBean(){
+		System.out.println("Not production bean created. Environment: " + environment.getProperty("myapplication.environment"));
+		return "Not production bean";
 	}
 
 //	@Bean
